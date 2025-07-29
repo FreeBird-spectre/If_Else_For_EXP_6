f46b61c033ed9a4042f90b01ce1e1eff99a73e6e
FreeBird-spectre <sethabir699@gmail.com>
2025-07-30 00:15:20 +0530

    Create 2 4 6 8..cpp

---
+++ 2 4 6 8..cpp
@@ -0,0 +1,22 @@
+// Abir Seth
+// EnTC A1
+// PRN-24070123003
+
+include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    for (int i=0;i<=10;i++)
+    {
+        if (i%2 == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
+
+0 2 4 6 8 10 
+
+=== Code Execution Successful ===
