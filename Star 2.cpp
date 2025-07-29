69d09f2601451a0a01733d036477b18fc335e827
FreeBird-spectre <sethabir699@gmail.com>
2025-07-30 00:18:05 +0530

    Create Star 2.cpp

---
+++ Star 2.cpp
@@ -0,0 +1,27 @@
+// Abir Seth
+// EnTC A1
+// PRN-24070123003
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    int n=6;
+    for (i=n;i>=1;i--)
+    {
+        for (j=1;j<=i;j++)
+        cout<<" * ";
+        cout<<endl;
+    }
+}
+
+*  *  *  *  *  *
+ *  *  *  *  *
+ *  *  *  *
+ *  *  *
+ *  *
+ *
+
+=== Code Execution Successful ===
