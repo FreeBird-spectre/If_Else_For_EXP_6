4802d845bb813265437bd0e027ebe8fa1a53d98b
FreeBird-spectre <sethabir699@gmail.com>
2025-07-30 00:13:50 +0530

    Create For A.cpp

---
+++ For.cpp
@@ -0,0 +1,23 @@
+// Abir Seth
+// Emtc A1
+// PRN-24070123003
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    for (int i=0;i<5;i++)
+    {
+     cout << "SIT" << endl;
+    }
+    return 0;
+}
+
+SIT
+SIT
+SIT
+SIT
+SIT
+
+=== Code Execution Successful ===
